f7118181526743b12ef99d5a800bcde5c8f66517
msakare <113922228+msakare@users.noreply.github.com>
2023-08-31 16:09:01 +0530

    Update apicall.py

--- .github/workflows/apicall.py
+++ .github/workflows/apicall.py
@@ -2,7 +2,7 @@ import requests
 url = "https://sagerx-aws-devtest-comm.cloud.databricks.com/api/2.1/jobs/list"
 payload = ""
 headers = {
-  'Authorization': 'Bearer dapic100eda776087528cd6a82f7ca84914a'
+  'Authorization': 'Bearer ${{ secrets.DATABRICKS_API_TOKEN }}'
 }
 response = requests.request("GET", url, headers=headers, data=payload)
 print(response.text)
