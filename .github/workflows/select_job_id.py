67de78a4adb28cdf53fec5dcf6cbdcf3af64480b
msakare <113922228+msakare@users.noreply.github.com>
2023-09-01 15:14:29 +0530

    Update select_job_id.py

--- .github/workflows/select_job_id.py
+++ .github/workflows/select_job_id.py
@@ -1,10 +1,12 @@
 import requests
 
+databricks_api_token = "dapic100eda776087528cd6a82f7ca84914a"
+
 url = "https://sagerx-aws-devtest-comm.cloud.databricks.com/api/2.1/jobs/get?job_id=496296034538647"
 
 payload={}
 headers = {
-  'Authorization': 'Bearer dapic100eda776087528cd6a82f7ca84914a’
+  'Authorization': f'Bearer {databricks_api_token}'
 }
 
 response = requests.request("GET", url, headers=headers, data=payload)
