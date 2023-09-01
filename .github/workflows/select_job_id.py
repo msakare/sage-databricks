5f809309b5aebce31a1a28cadfde657eb2e7c336
msakare <113922228+msakare@users.noreply.github.com>
2023-09-01 19:34:33 +0530

    Update select_job_id.py

--- .github/workflows/select_job_id.py
+++ .github/workflows/select_job_id.py
@@ -1,15 +1,50 @@
-import requests
+# import requests
 
-databricks_api_token = "dapic100eda776087528cd6a82f7ca84914a"
+# databricks_api_token = "dapic100eda776087528cd6a82f7ca84914a"
 
-url = "https://sagerx-aws-devtest-comm.cloud.databricks.com/api/2.1/jobs/get?job_id=496296034538647"
+# url = "https://sagerx-aws-devtest-comm.cloud.databricks.com/api/2.1/jobs/get?job_id=496296034538647"
 
-payload={}
-headers = {
-  'Authorization': f'Bearer {databricks_api_token}'
-}
+# payload={}
+# headers = {
+#   'Authorization': f'Bearer {databricks_api_token}'
+# }
 
-response = requests.request("GET", url, headers=headers, data=payload)
+# response = requests.request("GET", url, headers=headers, data=payload)
 
-print(response.text)
+# print(response.text)
+
+# select_job_id.py
+
+import sys
+import json
+
+# Read job IDs from the api_response.json artifact
+try:
+    with open('api_response.json', 'r') as file:
+        response_data = json.load(file)
+        job_ids = response_data.get('job_ids', [])
+except FileNotFoundError:
+    print("Error: api_response.json not found. Please run the previous steps to obtain job IDs.")
+    sys.exit(1)
+
+if not job_ids:
+    print("No job IDs found in the api_response.json artifact.")
+    sys.exit(1)
+
+# Display available job IDs to the user
+print("Available Job IDs:")
+for i, job_id in enumerate(job_ids, start=1):
+    print(f"{i}. {job_id}")
+
+# Prompt the user to select a job ID
+try:
+    selected_index = int(input("Enter the number corresponding to the desired Job ID: ")) - 1
+    if 0 <= selected_index < len(job_ids):
+        selected_job_id = job_ids[selected_index]
+        print(f"Selected Job ID: {selected_job_id}")
+    else:
+        print("Invalid selection. Please enter a valid number.")
+except ValueError:
+    print("Invalid input. Please enter a valid number.")
+
 
