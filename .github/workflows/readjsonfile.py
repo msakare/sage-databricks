97a88b2ddbc8017beeace9f8cc5d6fce94892aa5
msakare <113922228+msakare@users.noreply.github.com>
2023-09-05 12:43:14 +0530

    Create readjsonfile.py

---
+++ .github/workflows/readjsonfile.py
@@ -0,0 +1,16 @@
+ import json
+ 
+# Opening JSON file
+f = open('./api_response.json')
+ 
+# returns JSON object as
+# a dictionary
+data = json.load(f)
+print(data)
+# Iterating through the json
+# list
+for i in data['job_id']:
+    print(i)
+ 
+# Closing file
+f.close()
