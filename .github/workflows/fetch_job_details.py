38a7358dfa20ec65639eee5defa0099d6fdc6025
msakare <113922228+msakare@users.noreply.github.com>
2023-09-08 14:47:48 +0530

    Update fetch_job_details.py

--- .github/workflows/fetch_job_details.py
+++ .github/workflows/fetch_job_details.py
@@ -1,17 +1,18 @@
 import requests
 
-selected_job_id = input("Select a job ID: ")  # Input from the user
+databricks_api_token = "dapic100eda776087528cd6a82f7ca84914a"
+url = "https://sagerx-aws-devtest-comm.cloud.databricks.com/api/2.1/jobs/get?job_id=496296034538647"
 
-url = f"https://sagerx-aws-devtest-comm.cloud.databricks.com/api/2.1/jobs/get?job_id={selected_job_id}"
-
-payload = {}
+payload={}
 headers = {
-    'Authorization': 'Bearer <TOKEN>'  # Replace <TOKEN> with your token
+  'Authorization': f'Bearer {databricks_api_token}’
 }
 
 response = requests.request("GET", url, headers=headers, data=payload)
 
 print(response.text)
+
+
 
 
 # import sys
