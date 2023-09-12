d83a77c43c030a383b4c08ee18ee9fbe48d3d48c
msakare <113922228+msakare@users.noreply.github.com>
2023-09-12 15:16:54 +0530

    Update extract_job_ids.py

--- .github/workflows/extract_job_ids.py
+++ .github/workflows/extract_job_ids.py
@@ -1,13 +1,23 @@
-import json
-import sys
+import requests
+import os
 
-# Read the job list JSON from stdin
-job_list_json = sys.stdin.read()
+# Get the selected source environment from GitHub Actions environment variables
+source_environment = os.getenv("SOURCE")
 
-# Parse the JSON string to a Python dictionary
-job_list = json.loads(job_list_json)
+# Replace with your Databricks API token stored as a GitHub secret
+databricks_token = os.getenv("DATABRICKS_TOKEN")
 
-# Extract and print the job IDs
-job_ids = [job["job_id"] for job in job_list["jobs"]]
-for job_id in job_ids:
-    print(job_id)
+# Databricks API URL for Get Jobs List
+url = f"https://sagerx-aws-devtest-comm.cloud.databricks.com/api/2.1/jobs/list""
+
+headers = {
+    'Authorization': f'Bearer {databricks_token}'
+}
+
+response = requests.get(url, headers=headers)
+if response.status_code == 200:
+    job_list_json = response.text  # Get the JSON response
+    print(job_list_json)  # Print the JSON response for debugging
+    # Process the job list as needed
+else:
+    print(f"Failed to get job list. Status code: {response.status_code}")
