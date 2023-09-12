ec066f1475d8ab84ca16d8d788050cf50ee12d0d
msakare <113922228+msakare@users.noreply.github.com>
2023-09-12 14:09:40 +0530

    Create get_job_list.py

---
+++ .github/workflows/get_job_list.py
@@ -0,0 +1,22 @@
+import requests
+import os
+
+# Get the selected source environment from GitHub Actions environment variables
+source_environment = os.getenv("SOURCE")
+
+# Replace with your Databricks API token stored as a GitHub secret
+databricks_token = os.getenv("DATABRICKS_TOKEN")
+
+# Databricks API URL for Get Jobs List
+url = f"https://aws-{source_environment.lower()}test-comm.cloud.databricks.com/api/2.1/jobs/list"
+
+headers = {
+    'Authorization': f'Bearer {databricks_token}'
+}
+
+response = requests.get(url, headers=headers)
+if response.status_code == 200:
+    job_list = response.json()["jobs"]
+    # Process the job list as needed
+else:
+    print(f"Failed to get job list. Status code: {response.status_code}")
